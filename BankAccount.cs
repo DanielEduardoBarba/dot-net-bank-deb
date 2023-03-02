c7ac29bb154ea529c648d6cec138a94131c88050
Daniel Barba <frdb103@gmail.com>
2023-03-02 14:52:36 -0500

    using classes

---
+++ BankAccount.cs
@@ -0,0 +1,8 @@
+public class BankAccount
+{
+    public string customerName = "Bob";
+    public double balance = 300.01;
+    public string typeOfAccount = "checking";
+
+
+}
--- program.cs
+++ program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+BankAccount newaccount = new BankAccount();
+
+if(newaccount.typeOfAccount == "checking") newaccount.balance += 100;
+
+Console.WriteLine(newaccount.customerName+"'s balance is " + newaccount.balance);
